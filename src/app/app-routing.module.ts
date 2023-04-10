c8ad1cfa0f23a93732ffb73b3def7de082ff79e9
angedev07 <angelgaran20@gmail.com>
2023-04-10 15:58:23 -0500

    Creado rutas para el auth

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./modules/auth/auth.module').then(a => a.AuthModule)
+  },
+  {
+    path: 'reserva'
+  },
+  {
+    path: '**',
+    redirectTo: 'auth'
+  },
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
