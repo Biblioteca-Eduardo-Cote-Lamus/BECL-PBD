0c3b8a236ae99eb3933ab6e7da92dab0acd03131
angedev07 <angelgaran20@gmail.com>
2023-09-27 10:50:37 -0500

    fix(auth.guard): Se cambia la validacion de inicio de sesion por roles en el auth guard.

---
+++ src/app/data/const/roles.const.ts
@@ -0,0 +1,17 @@
+export const VALID_ROLES = {
+    COMMONT_USER: [
+        'TIC',
+        'FUNCIONARIO',
+        'ADMINISTRATIVO',
+        'DOCENTE'
+    ],
+    ADMINISTRADOR: 'ADMINISTRADOR'
+}
+
+// [
+//     'TIC',
+//     'FUNCIONARIO',
+//     'ADMINISTRATIVO',
+//     'DOCENTE',
+//     'ADMINISTRADOR'
+// ]
--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {CanActivate, CanActivateChild, Router} from '@angular/router';
 import { Observable } from 'rxjs';
+import { VALID_ROLES } from 'src/app/data/const/roles.const';
 import { AuthService } from 'src/app/modules/auth/services/auth.service';
 
 @Injectable({
@@ -32,16 +33,19 @@ export class AuthGuard implements CanActivate ,CanActivateChild{
   canActivateChild(): Observable<boolean> | boolean {
     const {user_rol} = JSON.parse(localStorage.getItem('user')!) || {}
 
-    if( this.authService.verifyIsUserLogin() && user_rol === 'Usuario'){
-      return true
+    if( !this.authService.verifyIsUserLogin()){
+      return false
     }
+    
+    if(VALID_ROLES.COMMONT_USER.includes(user_rol.toUpperCase()))
+      return true
 
-    if(user_rol === 'Administrador')
+    if(user_rol.toLowerCase() === VALID_ROLES.ADMINISTRADOR.toLowerCase())
       this.router.navigate(['/admin'])
-    else
-      this.router.navigate(['/auth'])
+    // else
+    //   this.router.navigate(['/auth'])
 
-    return false;
+    return true;
   }
 
 }
